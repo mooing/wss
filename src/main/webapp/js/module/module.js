ab2c748b25663c51c6ec1d94f92dfcee9378d9d1
zhiyan <zhiyan1987@gmail.com>
2013-12-10 22:32:35 +0800

    tree init

--- src/main/webapp/js/module/module.js
+++ src/main/webapp/js/module/module.js
@@ -9,6 +9,11 @@ function toTreeObj( arr ){
 		obj.id = v.id;
 		obj.text = v.name;
 		obj.state = "closed";
+		obj.attributes = {
+			parentName:"",
+			pId : 0,
+			isleaf:true
+		};
 		obj.children = [];
 		res.push( obj );
 	});
@@ -22,20 +27,114 @@ function toTreeObj( arr ){
 		obj.text = v.name;
 		obj.children = [];
 		pid = +v.pId;
+		obj.attributes = {
+			parentName:"",
+			pId:pid,
+			isleaf:true
+		};
 		$(res).each(function(){
 			if( +this.id === pid){
+				obj.attributes.parentName = this.text;
+				this.attributes.isleaf = false;
 				this.children.push( obj );
 			}
 		});
 	});
 	return res;
 }
+function putDataToForm( data, url ){
+	var $form = $("#module-form");
+	$form.find("input[name='pId']").val(data.pId);
+	$form.find("input[name='name']").val(data.name);
+	$form.find("input[name='id']").val(data.id);
+	$form.find(".pname").text(data.pname);
+	$form.attr("action",url);
+}
 
-$.getJSON("/module/alltree?guid="+new Date().getTime(),function(data){
-	data = toTreeObj( data );
-	$('#module-tree').tree({
-	    "data":data
+// 添加根节点
+$(".module-addroot").on("click",function(){
+	var data = {};
+	data.pId = "";
+	data.pname = "";
+	data.name = "";
+	data.id = "";
+	putDataToForm(data,'/module/addnode'+'?guid='+new Date().getTime());
+});
+
+// 删除
+$(".module-del").on("click",function(){
+	var node = $('#module-tree').tree('getSelected'),
+		isleaf = node.attributes.isleaf,
+		data = {};
+	if(!isleaf){
+		alert("非叶子节点无法删除");
+	}else{
+	    data.pId = node.attributes.pId;
+	    data.pname = node.attributes.parentName;
+	    data.name = node.text;
+	    data.id = node.id;
+		putDataToForm(data,'/module/delnode/'+node.id+'/?guid='+new Date().getTime() );
+		$('#module-form').form('submit', {
+			onSubmit: function(){
+				var isValid = $(this).form('validate');
+				return isValid;	
+			},
+			success: function(data) {
+				if(data){
+					alert("删除成功");
+					loadTree();
+				}
+			}
+		});
+	}
+	return false;
+});
+
+// 添加
+$(".module-add").on("click",function(){
+	var node = $('#module-tree').tree('getSelected'),
+		isleaf = node.attributes.isleaf,
+		data = {};
+	data.pId = node.attributes.pId;
+	data.pname = node.attributes.parentName;
+	data.name = "";
+	data.id = "";
+	putDataToForm(data,'/module/addnode?guid='+new Date().getTime() );
+	return false;
+});
+
+// 保存
+$(".module-save").on("click",function(){
+	$('#module-form').form('submit', {
+		onSubmit: function(){
+			var isValid = $(this).form('validate');
+			return isValid;	
+		},
+		success: function(data) {
+			if(data){
+				alert("保存成功");
+				loadTree();
+			}
+		}
 	});
 });
 
+// 初始化
+function loadTree(){
+	$.getJSON("/module/alltree?guid="+new Date().getTime(),function(data){
+		data = toTreeObj( data );
+		$('#module-tree').tree({
+		    "data":data,
+		    onClick: function(node){
+		    	var data = {};
+		    	data.pId = node.attributes.pId;
+		    	data.pname = node.attributes.parentName;
+		    	data.name = node.text;
+		    	data.id = node.id;
+		    	putDataToForm(data,'/module/updatenode'+'?guid='+new Date().getTime());
+			}
+		});
+	});
+}
 
+loadTree();
