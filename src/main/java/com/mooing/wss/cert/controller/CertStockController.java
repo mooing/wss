63ff615f2b39c491418a4049619404c8e3577b95
mooing <chimooing@163.com>
2013-12-27 00:19:24 +0800

    update

---
+++ src/main/java/com/mooing/wss/cert/controller/CertStockController.java
@@ -0,0 +1,21 @@
+package com.mooing.wss.cert.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mooing.wss.common.controller.BaseController;
+
+/**
+ * 
+ * 出生证库存 入库 申请
+ * 
+ * 
+ * @author kaiming.chi
+ * 
+ * @date 2013-12-26 下午11:40:01
+ */
+@Controller
+@RequestMapping("/certstock")
+public class CertStockController extends BaseController {
+
+}
