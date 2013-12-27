b8cf2f22fe6ffe5021e8dcc1f7eec9d337fe0215
zhiyan <zhiyan1987@gmail.com>
2013-12-27 11:14:13 +0800

    顶导航

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -18,11 +18,22 @@ var util = {
 // main
 $(function() {
 
+    // 测边菜单
     $(".menu-link").on("click", function() {
         var url = $(this).attr("href"),
             title = $(this).attr("title");
         util.addTab(title, url);
         return false;
+    });
+
+    // 顶导
+    $("#navMenu a").on("click",function(){
+        var title = $(this).text(),
+            rel = $(this).attr("rel");
+        $("#navMenu li").removeClass("selected");
+        $(this).closest("li").addClass("selected");
+        $("#sidebar .toggleCollapse h2").text( title );
+        $("#sidebar .accordionContent").hide().filter("[rel="+rel+"]").show();
     });
 
 });
